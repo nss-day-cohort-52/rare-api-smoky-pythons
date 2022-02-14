4d05bc05196e7f38636b0bf4e6c5f1b957f55e69
luciidlou <lewisale96@gmail.com>
2022-02-14 10:54:59 -0600

    implemented Post, Reaction, and PostReaction models

---
+++ rareapi/models/post_reaction.py
@@ -0,0 +1,8 @@
+from posixpath import realpath
+from django.db import models
+from models import Post, Reaction
+
+class PostReaction(models.Model):
+    user = models.ForeignKey(RareUser, on_delete=models.CASCADE, related_name='user')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post')
+    reaction = models.ForeignKey(Reaction, on_delete=models.CASCADE, related_name='reaction')
