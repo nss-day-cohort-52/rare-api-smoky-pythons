d19f461e9bf8981b6ee3a813d09842288f5533e7
lacizeidler <laci.zeidler@gmail.com>
2022-02-14 11:24:10 -0600

    added auto_now_add to dateField

--- rareapi/models/rare_user.py
+++ rareapi/models/rare_user.py
@@ -5,6 +5,6 @@ from django.contrib.auth.models import User
 class RareUser(models.Model):
     bio = models.CharField(max_length=200)
     profile_image_url = models.URLField()
-    created_on = models.DateField()
+    created_on = models.DateField(auto_now_add=True)
     active = models.BooleanField()
     user = models.OneToOneField(User, on_delete=models.CASCADE)
