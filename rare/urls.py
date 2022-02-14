b3038240d9c139439f07146c8e03bc0aea1d7f2a
jkuns76 <jkoonman@gmail.com>
2022-02-14 14:22:37 -0600

    views made, urls setup, and init setup up for comments, categories, demotionqueue, and subscriptions

--- rare/urls.py
+++ rare/urls.py
@@ -14,8 +14,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
+from rest_framework import routers
+from rareapi.views import register_user, login_user
+from rareapi.views import CategoryView
+from rareapi.views import SubscriptionView
+from rareapi.views import CommentView
+from rareapi.views import DemotionQueueView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'categories', CategoryView, 'category')
+router.register(r'comments', CommentView, 'comment')
+router.register(r'subscriptions', SubscriptionView, 'subscription')
+router.register(r'demotionQueues', DemotionQueueView, 'demotionQueue')
 
 urlpatterns = [
+    path('register', register_user),
+    path('login', login_user),
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
---
+++ rareapi/views/__init__.py
@@ -0,0 +1,6 @@
+
+
+from .category import CategoryView
+from .comment import CommentView
+from .subscription import SubscriptionView
+from .demotionQueue import DemotionQueueView
