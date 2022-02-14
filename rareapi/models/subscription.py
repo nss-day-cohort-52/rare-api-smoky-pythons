d9b704d5235995197d08bce8891bc8fbce314177
jkuns76 <jkoonman@gmail.com>
2022-02-14 11:24:19 -0600

    fixed auto add on date

--- rareapi/models/subscription.py
+++ rareapi/models/subscription.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Subscription(models.Model):
-    created_on = models.DateField()
-    ended_on = models.DateField
+    created_on = models.DateField(auto_now_add=True)
+    ended_on = models.DateField(auto_now_add=True)
     author_id  = models.ForeignKey("RareUser", on_delete=models.CASCADE)
     follower_id = models.ForeignKey("RareUser", on_delete=models.CASCADE)
