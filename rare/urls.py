290881e5e967f2a41b829da3067910892a9d5dc8
luciidlou <lewisale96@gmail.com>
2022-02-14 14:05:06 -0600

    can now successfully create a new user in the client

--- rare/urls.py
+++ rare/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from rest_framework import routers
+from rareapi.views import register_user, login_user
+
+router = routers.DefaultRouter(trailing_slash=False)
 
 urlpatterns = [
+    path('register', register_user),
+    path('login', login_user),
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
+    path('', include(router.urls))
 ]
+
+
