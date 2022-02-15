b53b39196889c1344204ee7746e5785c1e9a987d
lacizeidler <laci.zeidler@gmail.com>
2022-02-15 09:42:42 -0600

    fixtures

--- rareapi/views/category.py
+++ rareapi/views/category.py
@@ -20,6 +20,24 @@ class CategoryView(ViewSet):
         serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data)
     
+    def create(self, request):
+        category = Category.objects.create(
+            label = request.data['label']
+        )
+        serializer = CategorySerializer(category)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    def update(self, request, pk):
+        category = Category.objects.get(pk=pk)
+        category.label = request.data['label']
+        category.save()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+    
+    def destroy(self, request, pk):
+        category = Category.objects.get(pk=pk)
+        category.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+    
     
 class CategorySerializer(serializers.ModelSerializer):
 
