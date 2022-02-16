24ed43686506baced271581b79c2a8630683692a
luciidlou <lewisale96@gmail.com>
2022-02-16 15:49:54 -0600

    full CRUD functionality for posts and comments

--- rareapi/views/__init__.py
+++ rareapi/views/__init__.py
@@ -1,8 +1,8 @@
 from .auth import register_user, login_user
 from .post import PostView
-from .rare_user import RareUser
-from .reaction import Reaction
-from .tag import Tag
+from .rare_user import RareUserView
+from .reaction import ReactionView
+from .tag import TagView
 from .category import CategoryView
 from .comment import CommentView
 from .subscription import SubscriptionView
